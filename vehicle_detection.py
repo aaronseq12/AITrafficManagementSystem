73063813a472727a99d3c9d5e64a53369450de7c
Aaron Sequeira <96731649+aaronseq12@users.noreply.github.com>
2023-11-09 11:40:11 +0300

    Update vehicle_detection.py

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -16,9 +16,7 @@ outputPath = os.getcwd() + "/output_images/"
 def detectVehicles(filename):
    global tfnet, inputPath, outputPath
    img=cv2.imread(inputPath+filename,cv2.IMREAD_COLOR)
-   # img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
    result=tfnet.return_predict(img)
-   # print(result)
    for vehicle in result:
       label=vehicle['label']   #extracting label
       if(label=="car" or label=="bus" or label=="bike" or label=="truck" or label=="rickshaw"):    # drawing box and writing label
